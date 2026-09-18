2a697d3c2bf39c596cf4459ba7fb020c5a812c8c
coderskm <sumitkumarmishra.code@gmail.com>
2026-09-18 20:16:26 +0530

    Day18-18-Sept-2026

---
+++ Coding Practice/Day18-18-Sept-2026/Part-2/shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+greet(){
+    echo "welcome to pathnex devops training"
+}
+
+greet
---
+++ Coding Practice/Day18-18-Sept-2026/Part-3/terraform.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "PathnexEC2" {
+    ami           = "ami-0abcd1234abcd1234"
+    instance_type = "c6a.12xlarge"
+
+    user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd
+    EOF
+
+    tags = {
+        Name = "Pathnex-EC2"
+    }
+}
